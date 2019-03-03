5c41b24bb532ca748f9bb9697120a8c5899ff2e0
Norbert Schultz <norbert.schultz@reactivecore.de>
2019-03-03 21:23:28 +0100

    Adding file cache option

--- image_copy.py
+++ image_copy.py
@@ -9,16 +9,36 @@ import shutil
 parser = argparse.ArgumentParser(description='Copy Image Files into nob13 Format')
 parser.add_argument('--input', dest='input', required=True, help='Input Directory')
 parser.add_argument('--output', dest='output', required=True, help='Output Directory')
+parser.add_argument('--cache', dest='cache_file', help='Touched file cache')
 parser.add_argument('-s', help='simulate', action='store_true')
 
 args = parser.parse_args()
 print("Input  =   ", args.input)
 print("Output =   ", args.output)
 print("Simulate = ", args.s)
+print("Cache file=", args.cache_file)
+
+touched_files = set()
+if args.cache_file is not None:
+    try:
+        with open(args.cache_file, "r") as f:
+            touched_files = set(f.read().splitlines())
+    except:
+        print("Could not read cache file")
 
 if not os.path.isdir(args.output):
     print("Output is not a directory")
     exit(1)
+
+cache_writer = None
+
+if args.cache_file is not None:
+    cache_writer = open(args.cache_file, "a+")
+
+def add_to_cache(file):
+    if cache_writer is not None:
+        cache_writer.write(file)
+        cache_writer.write("\n")
 
 def get_exif_creation_date(filename: str) -> datetime.date:
     with open(file, "rb") as f:
@@ -55,14 +75,20 @@ def get_destination_dir(filename, file_date: datetime.date):
 for root, dirs, files in os.walk(args.input):
     for name in files:
         file = os.path.join(root, name)
+        if file in touched_files:
+            print(file, "Already in touched files")
+            continue
+
         file_date = get_file_date(file)
         destination_dir = get_destination_dir(file, file_date)
         destination_file = os.path.join(destination_dir, os.path.basename(file))
         # print("File ", file, " Date ", file_date, " ---> ", destination_file)
 
+
         if os.path.exists(destination_file):
             if filecmp.cmp(file, destination_file):
                 print("File ", file, " already exists, same content")
+                add_to_cache(file)
             else:
                 print("File differs!!!!")
         else:
@@ -71,3 +97,7 @@ for root, dirs, files in os.walk(args.input):
                 if not os.path.isdir(destination_dir):
                     os.makedirs(destination_dir)
                 shutil.copy2(file, destination_file)
+                add_to_cache(file)
+
+if cache_writer is not None:
+    cache_writer.close()
