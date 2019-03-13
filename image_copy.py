b1e63d2bdcfbf13c42c0b91186525e4923211295
Norbert Schultz <norbert.schultz@reactivecore.de>
2019-03-13 20:48:54 +0100

    Extensions for DNG Files of Alpha Camera

--- image_copy.py
+++ image_copy.py
@@ -11,12 +11,15 @@ parser.add_argument('--input', dest='input', required=True, help='Input Director
 parser.add_argument('--output', dest='output', required=True, help='Output Directory')
 parser.add_argument('--cache', dest='cache_file', help='Touched file cache')
 parser.add_argument('-s', help='simulate', action='store_true')
-
+parser.add_argument('--ext', dest='ext', help='Extension filter (with dot!)')
+parser.add_argument('--dng', dest='dng', help='DNG Hack (Check for existing DNG and skip)', action='store_true')
 args = parser.parse_args()
-print("Input  =   ", args.input)
-print("Output =   ", args.output)
-print("Simulate = ", args.s)
-print("Cache file=", args.cache_file)
+print("Input  =         ", args.input)
+print("Output =         ", args.output)
+print("Simulate =       ", args.s)
+print("Cache file=      ", args.cache_file)
+print("Extension filter=", args.ext)
+print("DNG Hack        =", args.dng)
 
 extensions = {'.jpg', '.jpeg', '.arw', '.mp4', '.avi'}
 
@@ -54,10 +57,11 @@ def get_exif_creation_date(filename: str) -> datetime.date:
 def get_exif_tool_create_date(filename: str) -> datetime.date:
     jsoninfo = pyexifinfo.get_json(filename)
     cand = jsoninfo[0].get("QuickTime:MediaCreateDate", None)
+    if cand is None:
+        cand = jsoninfo[0].get("Media Create Date", None)
     if cand is not None:
         return datetime.datetime.strptime(cand, "%Y:%m:%d %H:%M:%S").date()
-    # print("JSON ", jsoninfo)
-    return jsoninfo.get("Media Create Date", None)
+    return None
 
 
 def get_file_date(filename: str) -> datetime.date:
@@ -68,7 +72,7 @@ def get_file_date(filename: str) -> datetime.date:
     if exif2 is not None:
         return exif2
     print("Skipping to modification time for", filename)
-    mtime = datetime.datetime.fromtimestamp(os.path.getmtime(filename)).time()
+    mtime = datetime.datetime.fromtimestamp(os.path.getmtime(filename)).date()
     return mtime
 
 def get_destination_dir(filename, file_date: datetime.date):
@@ -85,12 +89,26 @@ for root, dirs, files in os.walk(args.input):
         if ext.lower() not in extensions:
             print(file, "skipped, not in valid extensions")
             continue
+        if (args.ext is not None) and (ext.lower() != args.ext):
+            print(file, "skipped, not in extension filter")
+            continue
+        if os.path.basename(file).startswith("."):
+            print(file, "skipped as being hidden")
+            continue
 
         file_date = get_file_date(file)
         destination_dir = get_destination_dir(file, file_date)
         destination_file = os.path.join(destination_dir, os.path.basename(file))
         # print("File ", file, " Date ", file_date, " ---> ", destination_file)
 
+        if args.dng:
+            beginname, _ = os.path.splitext(os.path.basename(file))
+            cand2 = os.path.join(destination_dir, beginname + ".dng")
+            print("Checking ", cand2)
+            if os.path.exists(cand2):
+                print("File ", file, " already exists as DNG, skipping")
+                add_to_cache(file)
+                continue
 
         if os.path.exists(destination_file):
             if filecmp.cmp(file, destination_file):
